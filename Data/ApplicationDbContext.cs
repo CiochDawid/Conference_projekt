e430d644ab7b7f1e3be1ac1e042c4c8e34339373
Dawid Cioch <dawid.cioch99@o2.pl>
2026-06-19 20:12:19 +0200

    06. Configure ASP.NET Identity authentication

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Conference_projekt.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Conference_projekt.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
