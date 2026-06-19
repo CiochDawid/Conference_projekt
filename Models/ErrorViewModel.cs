1a758cd26ea5aa451cae89472910bf259563bd3a
Dawid Cioch <dawid.cioch99@o2.pl>
2026-06-19 19:26:57 +0200

    01. Initial ASP.NET Core MVC project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Conference_projekt.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
